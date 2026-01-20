078c599599862a8c9318b5ff978c9057ad172519
Brian Antunes <brian.antunes@yahoo.com>
2026-01-19 20:18:31 -0500

    Starting TCP protocol

--- main.go
+++ main.go
@@ -5,39 +5,59 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"os"
+	"net"
 )
 
-func getLinesChannel(f io.ReadCloser) <-chan string
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+	go func() {
+		defer f.Close()
+		defer close(out)
+		line := ""
+		for {
+			buffer := make([]byte, 8)
+			bytesRead, err := f.Read(buffer)
+			if err != nil {
+				break
+			}
+
+			buffer = buffer[:bytesRead]
+
+			if i := bytes.IndexByte(buffer, '\n'); i != -1 {
+				line += string(buffer[:i])
+				out <- line
+				buffer = buffer[i+1:]
+				line = ""
+			}
+
+			line += string(buffer)
+		}
+		if len(line) != 0 {
+			out <- line
+		}
+	}()
+	return out
+}
 
 func main() {
-	file, err := os.Open("messages.txt")
+	listener, err := net.Listen("tcp", ":42069")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	line := ""
+	defer listener.Close()
+
 	for {
-		buffer := make([]byte, 8)
-		bytesRead, err := file.Read(buffer)
-
-		buffer = buffer[:bytesRead]
-
-		if i := bytes.IndexByte(buffer, '\n'); i != -1 {
-			line += string(buffer[:i])
-			//fmt.Printf("read: %s\n", line)
-			buffer = buffer[i+1:]
-			line = ""
-		}
-
-		line += string(buffer)
-
+		conn, err := listener.Accept()
 		if err != nil {
-			if err == io.EOF {
-				fmt.Println("End of the line...")
-				break
-			}
-			log.Fatal(err)
+			log.Fatal("error", "error", err)
 		}
+
+		linesChan := getLinesChannel(conn)
+
+		for line := range linesChan {
+			fmt.Println(line)
+		}
+		fmt.Println("Connection to ", conn.RemoteAddr(), "closed")
 	}
 }
